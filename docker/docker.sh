70edde6d6663b3777078aa7041280c27d363c632
Mikhail Lukashov <michael.lukashov@gmail.com>
2022-11-07 16:34:15 +0300

    docker.sh build | login

--- docker/docker.sh
+++ docker/docker.sh
@@ -10,6 +10,9 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # echo script_dir: ${script_dir}
 project_dir=$(dirname "${script_dir}")
 # echo project_dir: ${project_dir}
+
+far2l_build()
+{
 pushd "${project_dir}"
 
 docker build -t far2l -f docker/Dockerfile \
@@ -20,3 +23,28 @@ docker build -t far2l -f docker/Dockerfile \
 docker run --rm far2l cat /far2l-nowx-static.tar.gz > far2l-nowx-static.tar.gz
 
 popd
+}
+
+far2l_login()
+{
+pushd "${project_dir}"
+
+docker run -it --rm --name=far2l \
+	--mount type=bind,source=${PWD},target=/far2l \
+	far2l \
+	bash -i
+
+popd
+}
+
+if [[ "${1-}" == "build" ]] ; then
+  far2l_build
+  exit
+fi
+
+if [[ "${1-}" == "login" ]] ; then
+  far2l_login
+  exit
+fi
+
+build
