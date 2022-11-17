7031e467af4ee006dc64b8fe621ffd7d617a80ad
Mikhail Lukashov <michael.lukashov@gmail.com>
2022-11-17 13:47:32 +0300

    changed: docker/Dockerfile.alpine docker/entry.sh

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-#cmake -DEACP=no -DUSEWX=no -DOPT_USE_STATIC_EXT_LIBS=TRUE -S /far2l -B /build-far2l -DVCPKG_ROOT=/vcpkg
-#cmake --build /build-far2l -- -j4
-#cmake --build /build-far2l --target install
-
 exec "$@"
